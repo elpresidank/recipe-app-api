f2d40bb0132e3f7d56c0626ad3c08f7635370e1b
elpresidank <benopp@addank.com>
2020-07-27 00:40:23 -0500

    Added image field to recipe model.

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # Admin URL's
@@ -23,4 +25,4 @@ urlpatterns = [
     # Project URL's
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
